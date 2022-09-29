f8c198d59ce93215a633a14047a0f504e7629d41
Archi-Paweu <p.s.wisniewski@gmail.com>
2022-09-29 17:00:13 +0200

    osobna klasa Guy

---
+++ Guy.cs
@@ -0,0 +1,40 @@
+﻿namespace GiveCash
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " ma " + Cash + " zł.");
+        }
+        public int GiveCash(int amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("Kwota " + amount + " nie jest poprawną kwotą!");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine("Nie ma wystarczających środków aby dać ci " + amount);
+                return 0;
+            }
+            Cash = Cash - amount;
+            return amount;
+        }
+        public void ReceiveCash(int amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("Nie przyjmę od Ciebie długu  wysokości " + amount);
+
+            }
+            else Cash = Cash + amount;
+        }
+
+
+    }
+}
